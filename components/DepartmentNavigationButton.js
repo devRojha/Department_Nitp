6acad3409feace1bb0e8a8e86e297f0d09a08ed6
devRojha <devraj227804@gamil.com>
2024-06-08 18:55:58 +0530

    responsive

--- components/DepartmentNavigationButton.js
+++ components/DepartmentNavigationButton.js
@@ -3,7 +3,7 @@
 export const DepartmentNavigationButton = ({Title , onClick})=>{
     return (
         <div className="flex justify-center mb-10">
-            <button onClick={onClick()} className="w-[80%] py-5 rounded-lg bg-amber-50 shadow-lg shadow-slate-600 text-red-950 font-semibold hover:bg-amber-100 active:text-white text-center">{Title}</button>
+            <button onClick={onClick()} className="w-[80%] py-5 rounded-lg bg-amber-50 shadow-lg shadow-slate-600 text-red-950 font-semibold hover:bg-amber-100 active:text-white text-center text-sm">{Title}</button>
         </div>
     )
 }
