3cb11f502f34fc2674c6d05be37b996665cb45ab
Devraj Kumar <devrajkumar@Devrajs-MacBook-Air.local>
2024-06-24 14:42:17 +0530

    adding hod

--- app/AllDepartment/EE/hod/page.js
+++ app/AllDepartment/EE/hod/page.js
@@ -1,10 +1,9 @@
 import Image from "next/image"
 
-
-function page(){
+function Page(){
     return (
-        <div className="bg-orange-50 flex flex-col p-10 max-sm:p-2 h-screen">  
-            <div className="flex flex-col  mb-16">
+        <div className="bg-orange-50 flex flex-col p-10 max-sm:p-2 h-screen">
+            <div className="flex flex-col mb-16">
                 <div>
                     <p className="text-red-900 text-3xl max-sm:text-2xl font-bold">
                         HOD
@@ -17,11 +16,28 @@ function page(){
                 </div>
             </div>
             {/* section 1 */}
-            <div className="w-full grid grid-cols-5 border border-red-500 h-[60%]">
-                <Image src={"https://lh3.googleusercontent.com/d/1Q0TgCyX0bpbtiKhYiqtDBQ0N_7JzwiQI=w320?authuser=0"} width={700} height={1000} alt="Loading.." className="col-span-2 rounded-lg bg-rose-200 h-full" />
-                <div className=" col-span-3 border">hii</div>
+            <div className="w-full grid grid-cols-5 border shadow-lg shadow-slate-500 h-[60%]">
+                <div className="col-span-2 flex justify-center items-center  rounded-lg">
+                    <Image 
+                        src={"https://lh3.googleusercontent.com/d/1Q0TgCyX0bpbtiKhYiqtDBQ0N_7JzwiQI=w320?authuser=0"} 
+                        width={350} 
+                        height={350} 
+                        alt="Loading.." 
+                        className="rounded-lg object-contain"
+                    />
+                </div>
+                <div className="col-span-3 p-4">
+                    <div>
+                        <div className="flex justify-center mb-10 text-2xl max-md:text-md max-sm:text-sm text-black font-bold">Head of Department</div>
+                        <div className="text-2xl max-md:text-md max-sm:text-sm font-bold text-red-900 mb-2">Dr. Rajiv Mandal</div>
+                        <div className="text-2xl max-md:text-md max-sm:text-sm mb-2 font-bold text-green-800">Professor</div>
+                        <div className="text-1xlmax-md:text-md max-sm:text-sm mb-2">Email: head.ee@nitp.ac.in</div>
+                        <div className="text-1xl max-md:text-md max-sm:text-sm">Contact: +114</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
-export default page
+
+export default Page
