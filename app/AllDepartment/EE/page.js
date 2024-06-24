915f470b0ca9f4581999d2d6821c293aa3964722
Devraj Kumar <113443021+devRojha@users.noreply.github.com>
2024-06-24 10:34:50 +0530

    changing department home page

--- app/AllDepartment/EE/page.js
+++ app/AllDepartment/EE/page.js
@@ -95,14 +95,15 @@ export default function EE() {
         <div className="p-10 max-sm:px-0 border border-red-700 bg-orange-50">
            {/* heading */}
            <div className="text-3xl max-sm:text-2xl max-sm:ml-2 font-bold text-red-900 mb-10">ELECTRICAL ENGINEERING</div>
-           {/* Section one */}
+           {/* Section 1 */}
            <div className="grid grid-cols-6 max-md:grid-cols-1">
                {/* Department Picture */}
                <div className="h-[500px] flex justify-start py-10 col-span-3 mr-4 max-sm:mr-0">
                    <Image src="/cseimg.png" className="rounded-lg max-sm:rounded-none shadow-lg shadow-slate-600 h-full" alt="Logo" width={700} height={1000}/>
                </div>
+
                {/* HOD */}
-               <div className="h-[500px] max-sm:h-[400px] flex justify-end py-10 col-span-3">
+               {/* <div className="h-[500px] max-sm:h-[400px] flex justify-end py-10 col-span-3">
                    <div className="bg-amber-50 rounded-lg shadow-lg shadow-slate-600 w-full py-8 px-8 grid grid-cols-2">
                        <Image src={"https://lh3.googleusercontent.com/d/1Q0TgCyX0bpbtiKhYiqtDBQ0N_7JzwiQI=w320?authuser=0"} width={700} height={1000} alt="Loading.." className="col-span-1  border rounded-lg bg-rose-200 h-full" />
                        <div className="col-span-1 flex justify-center">
@@ -115,42 +116,44 @@ export default function EE() {
                            </div>
                        </div>
                    </div>
+               </div> */}
+
+                {/* Notice  */}
+               <div className="h-[500px] py-10  col-span-3 max-lg:col-span-3 flex flex-col max-md:mb-10">
+                    <div className="bg-white overflow-hidden rounded-lg shadow-lg shadow-slate-600 px-2 ">
+                        <div className="flex justify-between text-lg font-semibold text-slate-500">
+                            <div>Important Notice</div>
+                            <button className="hover:text-blue-500">View All</button>
+                        </div>
+                        <div className="overflow-hidden space-y-2">
+                            {Notices.map((notice, id)=>{
+                                return (
+                                    <DepartmentNotify key={id} title={notice.notice} fun={notice.fun} />
+                                )
+                            })}
+                        </div>
+                    </div>
                </div>
            </div>
            {/* Section 2 */}
-           <div className="grid grid-cols-6 max-md:grid-cols-1">
-               {/* Notice  */}
-               <div className="bg-white px-2 py-4 h-[600px] rounded-lg shadow-lg shadow-slate-600 col-span-2 max-lg:col-span-3 flex flex-col max-md:mb-10">
-                   <div className="mb-6 flex justify-between text-lg font-semibold text-slate-500">
-                       <div>Important Notice</div>
-                       <button className="hover:text-blue-500">View All</button>
-                   </div>
-                   <div className="overflow-hidden space-y-2">
-                    {Notices.map((notice, id)=>{
-                        return (
-                            <DepartmentNotify key={id} title={notice.notice} fun={notice.fun} />
-                        )
-                    })}
-                   </div>
-               </div>
-               <div className="col-span-4 max-lg:col-span-3 flex flex-col justify-between ml-4">
-                   {/* About */}
-                   <div className="px-6 max-sm:px-2">
-                       <div className="mb-4 text-2xl max-sm:text-lg font-semibold text-red-950">About</div>
-                       <div className="text-justify max-sm:text-[12px]">The Department of Electrical Engineering in National Institute of Technology Patna (Formerly known as Bihar College of Engineering) was established in 1945 under Patna University with an intake of 45 students leading to a Bachelor of Science (Engineering) degree of four-year duration. In the year 1978 AICTE approved Post Graduate (PG) course leading to M.Sc. (Engg.) Degree in two specializations: (i) Power System Engineering and (ii) Control System Engineering. The department is also offering the Ph.D. research program since 2009. <button onClick={()=>router.push('/AllDepartment/EE/about')} className="text-blue-600">more...</button></div>
-                   </div>
-                   {/* Navigation Button  */}
-                   <div className="grid grid-cols-3 max-sm:grid-cols-2 mt-10">
-                       <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/mission')} Title={"MISSION AND VISSION"}/>
-                       <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/faculty')} Title={"FACULTY"}/>
-                       <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/labs')} Title={"LABS"}/>
-                       <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/syllabus')} Title={"SYLLABUS"}/>
-                       <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/activities')} Title={"ACTIVITIES"}/>
-                       <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/achivments')} Title={"ACHIEVMENTS"}/>
-                       <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/research')} Title={"RESEARCH HIGHLIGHT"}/>
-                       <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/usefullLinks')} Title={"USEFULL LINKS"}/>
-                   </div>
-               </div>
+           <div className="grid grid-cols-2 max-md:grid-cols-1">
+                {/* About */}
+                <div className="px-6 max-sm:px-2 ">
+                    <div className="mb-4 text-2xl max-sm:text-lg font-semibold text-red-950">About</div>
+                    <div className="text-justify max-sm:text-[12px]">The Department of Electrical Engineering in National Institute of Technology Patna (Formerly known as Bihar College of Engineering) was established in 1945 under Patna University with an intake of 45 students leading to a Bachelor of Science (Engineering) degree of four-year duration. In the year 1978 AICTE approved Post Graduate (PG) course leading to M.Sc. (Engg.) Degree in two specializations: (i) Power System Engineering and (ii) Control System Engineering. The department is also offering the Ph.D. research program since 2009. <button onClick={()=>router.push('/AllDepartment/EE/about')} className="text-blue-600">more...</button></div>
+                </div>
+                {/* Navigation Button  */}
+                <div className="grid grid-cols-3 max-sm:grid-cols-2 mt-10">
+                    <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/hod')} Title={"HOD Info"}/>
+                    <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/mission')} Title={"MISSION AND VISSION"}/>
+                    <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/faculty')} Title={"FACULTY"}/>
+                    <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/labs')} Title={"LABS"}/>
+                    <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/syllabus')} Title={"SYLLABUS"}/>
+                    <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/activities')} Title={"ACTIVITIES"}/>
+                    <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/achivments')} Title={"ACHIEVMENTS"}/>
+                    <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/research')} Title={"RESEARCH HIGHLIGHT"}/>
+                    <DepartmentNavigationButton onClick={()=>router.push('/AllDepartment/EE/usefullLinks')} Title={"USEFULL LINKS"}/>
+                </div>
            </div>
         </div>
        )
