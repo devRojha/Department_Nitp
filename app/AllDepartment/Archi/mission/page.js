becd3cfb04b7cab8741bed4b5a3250a2c3ddcbce
Siddhant <sidisan0711@gmail.com>
2024-07-07 00:05:28 +0530

    chem act image load error resolve

--- app/AllDepartment/Archi/mission/page.js
+++ app/AllDepartment/Archi/mission/page.js
@@ -2,7 +2,7 @@
 
 function Page(){
     return (
-        <div className="bg-orange-50 flex flex-col p-5 h-screen">  
+        <div className="bg-orange-50 flex flex-col p-5">  
             <div className="flex flex-col md:ml-10">
                 <div>
                     <p className="text-red-900 text-3xl font-bold">
