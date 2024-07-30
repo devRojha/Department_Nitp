30cff42c0f4c5e5e9fd304185ae448351ce76a4d
Nishant kumar <nishantkumarfps@gmail.com>
2024-07-30 15:00:53 +0530

    Updated the background color and the plain text of the mathematics deapartment

--- app/AllDepartment/Math/research/page.js
+++ app/AllDepartment/Math/research/page.js
@@ -3,7 +3,7 @@ import BackDepartment from "@/components/BackDepartment"
 
 function page(){
     return (
-      <div className="bg-orange-50 flex flex-col p-5 h-screen">
+      <div className=" flex flex-col p-5 h-screen">
         <div className="flex flex-col md:ml-10">
           <div>
             <p className="text-red-900 text-3xl font-bold">
