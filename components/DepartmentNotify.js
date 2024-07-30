257c6732683bb24847cfdd1dc4d16e5e871a3e61
Devraj Kumar <113443021+devRojha@users.noreply.github.com>
2024-07-30 19:54:25 +0530

    adding feature-picture

--- components/DepartmentNotify.js
+++ components/DepartmentNotify.js
@@ -4,11 +4,21 @@ function DepartmentNotify(props) {
     const link = props.link;
     if(link && link.length > 5){
         return (
-            <a href={`${link}`} className=" my-1 font-semibold text-[14px] flex text-start hover:text-blue-500">- {props.title}</a>
+            <a href={`${link}`} className=" my-1 font-semibold text-[14px] flex text-start hover:text-blue-500"> 
+                <div className="flex items-center">
+                    <div className="border rounded-full bg-black h-[10px] w-[10px] mr-2"></div> 
+                    {props.title}
+                </div>
+            </a>
         )
     }
     return (
-        <div className="my-1 flex text-start">- {props.title}</div>
+        <div className="my-1 flex text-start">
+            <div className="flex items-center">
+                <div className="border rounded-full bg-black h-[10px] w-[10px] mr-2"></div> 
+                {props.title}
+            </div>
+        </div>
     )
 
 }
