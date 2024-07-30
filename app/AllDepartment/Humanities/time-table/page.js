6f2408ea01e0d7d587c14afbef5c57cb60afbec7
Devraj Kumar <113443021+devRojha@users.noreply.github.com>
2024-07-30 16:09:28 +0530

    humanity style done

--- app/AllDepartment/Humanities/time-table/page.js
+++ app/AllDepartment/Humanities/time-table/page.js
@@ -16,9 +16,8 @@ function page() {
                     </p>
                 </div>
                 <BackDepartment navigate={"/AllDepartment/Humanities"} />
-
             </div>
-            <div className="max-sm:mx-6 max-md:mx-8 mx-20 border p-4 rounded-md shadow-lg shadow-slate-400 backdrop-blur-md">
+            <div className="max-sm:mx-6 max-md:mx-8 mx-20 border p-4 rounded-md shadow-lg shadow-slate-400 backdrop-blur-md h-screen">
                 {/* LINKS */}
                 <div className="mb-10">
                     <ul className="list-disc list-inside ml-5 space-y-4 text-lg max-md:text-[15px] max-sm:text-sm">
