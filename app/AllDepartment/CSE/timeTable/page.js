9879e18b4318a718a599661fbf5ab0e726da2b75
Devraj Kumar <devrajkumar@Devrajs-MacBook-Air.local>
2024-07-30 15:50:57 +0530

    cse style complete

--- app/AllDepartment/CSE/timeTable/page.js
+++ app/AllDepartment/CSE/timeTable/page.js
@@ -3,7 +3,7 @@ import BackDepartment from "@/components/BackDepartment"
 
 function Page(){
     return (
-        <div className="bg-orange-50 flex flex-col p-5 h-screen">  
+        <div className=" flex flex-col p-5 h-screen">  
             <div className="flex flex-col md:ml-10">
                 <div>
                     <p className="text-red-900 text-3xl font-bold">
@@ -18,8 +18,10 @@ function Page(){
                 <div>
                 <BackDepartment navigate={'/AllDepartment/CSE'}/>
                 </div>
-                <div className='mt-14'>
-                    <a href={'https://www.dropbox.com/scl/fo/kmdu6u1erwn18lwo0qe4p/h?rlkey=rl1xo6nzihg0j5yc9zvfidfr0&dl=0'} className='lg:text-lg text-gray-500 font-semibold border-b-2 border-black' target="_blank">
+            </div>
+            <div className="max-sm:mx-6 max-md:mx-8 mx-20 border p-4 rounded-md shadow-lg shadow-slate-400 backdrop-blur-md h-screen">
+                <div className='mt-14 '>
+                    <a href={'https://www.dropbox.com/scl/fo/kmdu6u1erwn18lwo0qe4p/h?rlkey=rl1xo6nzihg0j5yc9zvfidfr0&dl=0'} className='lg:text-lg text-gray-500 font-semibold border-b-2 border-black hover:text-blue-500' target="_blank">
                         Click here for the time table of CSED (for all semesters)
                     </a>
                 </div>
